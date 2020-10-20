3221a1d7c09691659d204323749b7a08117595e4
EdwardNewsky <EdwardNewsky@gmail.com>
2020-10-21 01:24:26 +0300

    чуть настроил слайдер

--- #src/js/swiper.js
+++ #src/js/swiper.js
@@ -1,11 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
-  direction: 'vertical',
+  // direction: 'vertical',
+  slidesPerView: 1,
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -14,8 +16,19 @@ var mySwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // Навигация
+  autoplay: {
+    delay: 3000,
   },
+
+  // Адаптивность - mobiles first
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  }
+
 })
--- dist/js/script.js
+++ dist/js/script.js
@@ -9426,12 +9426,14 @@ testWebP(function (support) {
 
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
-  direction: 'vertical',
+  // direction: 'vertical',
+  slidesPerView: 1,
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -9440,8 +9442,19 @@ var mySwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // Навигация
+  autoplay: {
+    delay: 3000,
   },
+
+  // Адаптивность - mobiles first
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  }
+
 })
