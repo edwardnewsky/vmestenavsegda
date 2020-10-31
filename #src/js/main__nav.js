d2d2826836f4a53a835ffc5bdc8af789745c1570
EdwardNewsky <EdwardNewsky@gmail.com>
2020-11-01 01:41:38 +0300

    Мелочи

--- #src/js/main__nav.js
+++ #src/js/main__nav.js
@@ -1,7 +1,14 @@
 var navMain = document.querySelector('.main-nav');
 var navBtn = document.querySelector('.main-nav__btn--menu');
-var fixZindex1 = document.querySelector('.header-offer__container1');
-var fixZindex2 = document.querySelector('.header-offer__container2');
+
+var navLink1 = document.querySelector('.js-1');
+var navLink2 = document.querySelector('.js-2');
+var navLink3 = document.querySelector('.js-3');
+var navLink4 = document.querySelector('.js-4');
+var navLink5 = document.querySelector('.js-5');
+var navLink6 = document.querySelector('.js-6');
+var navLink7 = document.querySelector('.js-7');
+
 // navMain.classList.remove('main-nav--nojs');
 
 navBtn.addEventListener('click', function() {
@@ -13,5 +20,48 @@ navBtn.addEventListener('click', function() {
 		navMain.classList.add('main-nav--closed');
 		navMain.classList.remove('main-nav--opened');
 
+	}
+});
+
+navLink1.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink2.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink3.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink4.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink5.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink6.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+});
+navLink7.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--opened')) {
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
 	}
 });
