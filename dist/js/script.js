298f077ef1afe67cc1d048b9b182da58fb91daf4
EdwardNewsky <EdwardNewsky@gmail.com>
2020-11-01 02:12:22 +0300

    thnx иван за меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navBtn = document.querySelector('.main-nav__btn--menu');
-
-var navLink1 = document.querySelector('.js-1');
-var navLink2 = document.querySelector('.js-2');
-var navLink3 = document.querySelector('.js-3');
-var navLink4 = document.querySelector('.js-4');
-var navLink5 = document.querySelector('.js-5');
-var navLink6 = document.querySelector('.js-6');
-var navLink7 = document.querySelector('.js-7');
+var navLinkList = document.querySelectorAll('.site-list__link');
 
 // navMain.classList.remove('main-nav--nojs');
 
@@ -23,48 +16,17 @@ navBtn.addEventListener('click', function() {
 	}
 });
 
-navLink1.addEventListener('click', function() {
+const onNavLinkClick = function() {
 	if (navMain.classList.contains('main-nav--opened')) {
 		navMain.classList.remove('main-nav--opened');
 		navMain.classList.add('main-nav--closed');
 	}
+};
+
+navLinkList.forEach(function(navLink) {
+	navLink.addEventListener('click', onNavLinkClick);
 });
-navLink2.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
-navLink3.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
-navLink4.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
-navLink5.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
-navLink6.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
-navLink7.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--opened')) {
-		navMain.classList.remove('main-nav--opened');
-		navMain.classList.add('main-nav--closed');
-	}
-});
+
 function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
