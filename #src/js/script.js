e9620c435398e727b17f73c4ce3c74e17456d8d0
EdwardNewsky <EdwardNewsky@gmail.com>
2020-11-02 01:57:26 +0300

    хочу добавить js lib отдельно

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,10 +3,5 @@
 @@include('swiper-bundle.min.js')
 @@include('swiper.js')
 @@include('anchor-scroll.js')
-@@include('counter.js')
 @@include('dates.js')
 @@include('lazy-ivan.js')
-@@include('video__fix.js')
-// Libs
-@@include('wow.min.js')
-@@include('wow-init.js')
